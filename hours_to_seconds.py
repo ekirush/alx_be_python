c6c774d42d578a6a720bec972ae76d491404d0e7
ekirush <ekirush966@gmail.com>
2025-05-19 23:27:30 +0300

    Introduction to python tasks

---
+++ hours_to_seconds.py
@@ -0,0 +1,6 @@
+# Convert Hours to Seconds
+
+hours = 2
+seconds =  60 * 60
+
+print(f"{hours} hour(s) is {seconds * hours} seconds")
---
+++ simple_interest.py
@@ -0,0 +1,12 @@
+# Simple Interest Calculator
+
+#Calculating simple interest (I = P * R * T) and storing result in variable namely interest
+principal = 1000
+rate = 0.05
+time = 3 
+
+# Simple Interest Calculation
+interest = principal * rate * time 
+
+# Results
+print(f"The simple interest is: {interest}")
