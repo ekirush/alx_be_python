f0169076060709b3c9c12ffed8dc93a4f8e6fc51
ekirush <ekirush966@gmail.com>
2025-05-20 16:51:31 +0300

    Task One: Basic Arithmetic Exercise

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,19 @@
+# Task One: Basic Arithmetic Exercise
 
+# Python script that performs basic arithmetic operations with two predefined numbers
+
+# Assign specific values to two variables
+number1 = 10
+number2 = 5
+
+# Calculate the sum, difference (by subtracting number2 from number1), and product of number1 and number2.
+addition = number1 + number2
+
+subtraction =  number1 - number2
+
+multiplication = number1 * number2
+
+# Calculation Results
+print(f"Addition of {number2} and {number2} is {addition}")
+print(f"Subtraction of {number2} and {number2} is {subtraction}")
+print(f"Multiplication of {number2} and {number2} is {multiplication}")
