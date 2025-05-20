59c8b5191df202637d5c0a98b0d2e3484524d99a
ekirush <ekirush966@gmail.com>
2025-05-20 17:41:27 +0300

    since there are 60 minutes in an hour and 60 seconds in a minute, thus 60 x 60 = 3600

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -2,10 +2,9 @@
 
 # Converts a specific number of hours into seconds
 hours = 2
-seconds_in_an_hour = 60 * 60 
 
-# Calculation
-seconds = hours * seconds_in_an_hour
+# Since there are 60 minutes in an hour and 60 seconds in a minute, thus 60 x 60 = 3600
+seconds = hours * 3600
 
 # Results
 print(f" {hours} hour(s) is {seconds} seconds")
