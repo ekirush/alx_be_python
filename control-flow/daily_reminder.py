50e2f78e9b8cbe8fe9b6b35b40ba2b24f96048ae
ekirush <ekirush966@gmail.com>
2025-05-29 22:10:17 +0300

    Personal Daily Reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,37 @@
+'''
+Personal Daily Reminder
+************************
+Objective: Create a simplified Python script that uses conditional statements, Match Case, and loops 
+to remind the user about a single, priority task for the day based on time sensitivity.
+******************************************************************************************************
+Task Description:
+
+Develop a script named daily_reminder.py. 
+This script will ask the user for a single task, its priority level, and if it is time-sensitive. 
+The program will then provide a customized reminder for that task, 
+demonstrating control flow and loops without relying on data structures to store multiple tasks.
+'''
+
+task = input('Enter Task Description: ').strip()
+priority = input('Enter Task Priority (high, medium, low): ').lower()
+time_bound = input('Is Task Time-bound? (yes or no): ').strip().lower()
+
+match priority:
+    case 'high':
+        message = f"{task} is a high priority task "
+    case 'medium':
+        message = f"{task} is a medium priority task "
+    case 'low':
+        message = f"{task} is a low priority task "
+    case _:
+        message = 'Enter Correct Priority'
+        
+if time_bound == 'yes':
+    message += "task that requires immediate attention today!"
+elif time_bound == 'no':
+    message += "consider completing it when you have free time."
+else:
+    message = ("Enter correct time bound")
+
+
+print(message)
