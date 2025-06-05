59cef78ebbec111c368df2e0b36088201f58dfbb
ekirush <ekirush966@gmail.com>
2025-06-05 12:08:10 +0300

    Temperature Conversion Tool

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,35 @@
+# Temperature Conversion Tool
+
+'''
+Objective: 
+Illustrate the concept of variable scope by creating a script that converts temperatures between Celsius and 
+Fahrenheit, using global variables to store conversion factors.
+'''
+
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+    celsius = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
+
+def convert_to_fahrenheit(celsius):
+    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
+    return fahrenheit
+
+# Prompt the user to enter a temperature and specify whether it’s in Celsius or Fahrenheit
+def temperature_display():
+    temperature = int(input("Enter the temperature to convert: "))
+    temp_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+    if temp_type == 'F':
+        conversion = convert_to_celsius(temperature)
+        print(f"{temperature}°F is {conversion}°C")
+    elif temp_type == 'C':
+        conversion = convert_to_fahrenheit(temperature)
+        print(f"{temperature}°C is {conversion}°F")
+    else:
+        print('Enter correct values')
+        
+temperature_display()
+
