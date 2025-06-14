c56a6bf10f5d218e3f05c027bafa45fd0299feba
ekirush <ekirush966@gmail.com>
2025-06-14 12:25:39 +0300

    Unit Tests Updated

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,24 +5,27 @@ import unittest
 class Test_Class(unittest.TestCase):
     def setUp(self):
         """Set up the SimpleCalculator instance before each test."""
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
         
         
     def test_add(self):
-        addition = self.calculator.add(5,3)
+        addition = self.calc.add(5,3)
         self.assertEqual(addition,8)
         
     def test_subtract(self):
-        subtraction = self.calculator.subtract(10,2)
+        subtraction = self.calc.subtract(10,2)
         self.assertEqual(subtraction,8)
+        
     def test_multiply(self):
-        multiply = self.calculator.multiply(10,2)
+        multiply = self.calc.multiply(10,2)
         self.assertEqual(multiply,20)
+        
     def test_divide(self):
-        division = self.calculator.divide(10,2)
+        division = self.calc.divide(10,2)
         self.assertEqual(division,5)
+        
     def test_divide_by_zero(self):
-        division = self.calculator.divide(10, 0)
+        division = self.calc.divide(10, 0)
         self.assertEqual(division, None)
 
 # if __name__ == '__main__':
