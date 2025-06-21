139b6a2620c3fdd47c9221736180b3e53e897cce
ekirush <ekirush966@gmail.com>
2025-06-21 11:58:03 +0300

    Python Magic Methods update

--- oop/book_class.py
+++ oop/book_class.py
@@ -8,16 +8,16 @@ In this script, define a Book class that uses specific magic methods to enhance 
 This class will model a book with attributes for the title, author, and publication year
 '''
 class Book:
-    def __init__(self, title:str, author:str, pub_year:int):
+    def __init__(self, title:str, author:str, year:int):
         self.title = title
         self.author = author
-        self.pub_year = pub_year
+        self.year = year
     
     def __del__(self):
         print(f"Deleting {self.title}")
     
     def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.pub_year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.pub_year})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
